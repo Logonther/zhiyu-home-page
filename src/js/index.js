92947e90477130c8fcec7e19770dc9a212eeb704
whlx <123>
2019-10-27 09:59:47 +0800

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,63 @@
+import '../css/bootstrap.min.css'
+// import './bootstrap.min.js'
+import 'swiper/css/swiper.min.css'
+import 'swiper/js/swiper.min.js'
+import 'font-awesome/css/font-awesome.min.css'
+import '../less/base.less'
+import '../less/index.less'
+import Swiper from 'swiper'
+
+$(function () {
+    var bannerSwiper = new Swiper ('.banner .swiper-container', {
+        width: window.innerWidth,
+        height: window.innerWidth * 685 / 1440,
+        speed: 1000,
+        loop: true,
+        /*autoplay: {
+            disableOnInteraction: false,
+        },*/
+        pagination: {
+            el: '.banner .swiper-pagination',
+            clickable: true
+        }
+    })
+    var solutionSwiper = new Swiper ('.solution .swiper-container', {
+        width: window.innerWidth,
+        height: window.innerWidth * 532 / 864 >= 532 ? 532 : window.innerWidth * 532 / 864,
+        loop: true,
+        speed: 1000,
+        direction: 'vertical',
+        autoplay: {
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.solution .solution-pagination .swiper-pagination',
+            clickable: true,
+            bulletClass : 'solution-bullet',
+            renderBullet: function (index, className) {
+                var text = ''
+                switch(index){
+                    case 0:text='运营监控';break;
+                    case 1:text='自主分账';break;
+                    case 2:text='物联网络';break;
+                    case 3:text='智能管家';break;
+                    case 4:text='系统集成';break;
+                }
+                return '<span class="' + className + '">' + text + '</span>';
+            }
+        },
+    })
+    if($('.how .pic').width()/$('.how .pic').height() > 1.5){
+        $('.how img').css({'width':'100%','height':'auto'})
+    }else{
+        $('.how img').css({'width':'auto','height':'100%'})
+    }
+
+    /*鼠标移入移出效果*/
+    $('.platform').on('mouseenter',function () {
+        $(this).find('.shade').toggleClass('henshin')
+    })
+    $('.platform').on('mouseleave',function () {
+        $(this).find('.shade').toggleClass('henshin')
+    })
+})
---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+let path = require('path');
+let webpack = require('webpack');
+var htmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry:{
+        index: './src/js/index',
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "js/index.js"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.ProvidePlugin({
+            "$": "jquery",
+            "jQuery": "jquery",
+            "window.jQuery": "jquery"
+        }),
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.join(__dirname,'./index.html'),
+            inject: 'body',//script标签的放置
+            minify: {//html压缩
+                removeComments: true,//移除注释
+                collapseWhitespace: true //移除空格
+            }
+        }),
+        new webpack.LoaderOptionsPlugin({
+            options: {
+                postcss: [
+                    require("autoprefixer")({
+                        browsers: ["last 5 versions"]
+                    })
+                ]
+            }
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: path.resolve(__dirname,'/src'),
+                exclude: path.resolve(__dirname,'/node_modules'),
+                options: {
+                    name: 'js/[name].[ext]'
+                }
+            },
+            //处理css中的loader
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            //处理sass中的loader
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!postcss-loader!less-loader'
+            },
+            {
+                test: /\.sass$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            //处理html模板中的loader
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            //处理图片中的loader( 通常url/file/image-webpack等loader配合 )
+            {
+                test: /\.(png|jpg|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: 'img/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg|woff|woff2)$/,
+                use: ['url-loader']
+            }
+        ]
+    }
+};
